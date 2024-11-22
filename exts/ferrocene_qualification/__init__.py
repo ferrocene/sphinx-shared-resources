7de064b785b3916e9f860ffc40582fce48eb2d73
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-11-22 12:54:38 -0800

    Manual update from ferrocene/ferrocene
    mirrored-commit: 79cfbf8b352b0611672aefbbe877d9584fc11d6c

--- exts/ferrocene_qualification/__init__.py
+++ exts/ferrocene_qualification/__init__.py
@@ -1,7 +1,15 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 # SPDX-FileCopyrightText: The Ferrocene Developers
 
-from . import substitutions, document_id, domain, signature_page, target
+from . import (
+    document_id,
+    domain,
+    intersphinx_support,
+    signature_page,
+    sphinx_needs_support,
+    substitutions,
+    target,
+)
 import string
 
 
@@ -11,16 +19,19 @@ def setup(app):
     domain.setup(app)
     signature_page.setup(app)
     target.setup(app)
+    intersphinx_support.setup(app)
+    sphinx_needs_support.setup(app)
 
     app.connect("config-inited", validate_config)
+    app.connect("config-inited", inject_version)
     app.add_config_value("ferrocene_id", None, "env", [str])
     app.add_config_value("ferrocene_substitutions_path", None, "env", [str])
     app.add_config_value("ferrocene_target_names_path", None, "env", [str])
     app.add_config_value("ferrocene_signature", None, "env", [str])
     app.add_config_value("ferrocene_private_signature_files_dir", None, "env", [str])
+    app.add_config_value("rustfmt_version", None, "env", [str])
     app.add_config_value("ferrocene_version", None, "env", [str])
     app.add_config_value("rust_version", None, "env", [str])
-    app.add_config_value("channel", None, "env", [str])
 
     return {
         "version": "0",
@@ -40,3 +51,11 @@ def validate_config(app, config):
 
     if any(c not in string.ascii_uppercase for c in config["ferrocene_id"]):
         raise ValueError("ferrocene_id can only be uppercase letters")
+
+
+def inject_version(app, config):
+    # sphinx-needs requires the document version to be configured in order for
+    # external needs to be loaded. Dynamically set it to the Ferrocene version
+    # if there is no existing version.
+    if not config.version and config.ferrocene_version:
+        config.version = config.ferrocene_version
